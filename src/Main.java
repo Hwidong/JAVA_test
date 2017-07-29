47b7ce1e5f3a1067623b104efbe5386e57336e16
gnleh <gnleh@DESKTOP-R3H93RA>
2017-07-30 00:30:21 +0900

    test2

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String [] test = new String[2];
-		
-		System.out.println(test.length);
+	
+		System.out.println("test");
 	}
-
 }
